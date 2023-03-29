f8c8ef9af31066302cf5623a6672ea1baaed4623
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-03-29 20:32:20 +0800

    update

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-//How randomly the computer will input
-function getComputerResponse() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
-}
+const getUserSelection = () => prompt("Choose your weapon").toLowerCase();
 
-//formulate an if statements of how randomly the player will choose the above options.
-const playerInput = prompt("Input rock, paper, or scissors.");
-if (
-  playerInput === "rock" ||
-  playerInput === "paper" ||
-  playerInput === "scissors"
-) {
-  console.log("playerInput");
-} else {
-  console.log("error! try again!");
-}
+const getComputerSelection = () => {
+  const weapons = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * weapons.length);
 
-// Declare Winner
-const winner = declareWinner(playerInput, computerInput);
-if (playerInput === "rock" && computerInput === "paper") {
-  console.log("Talo Ka! Tinalo ng Papel ang Bato");
-} else if (playerInput === "paper" && computerInput === "scissors") {
-  console.log("Talo Ka! Tinalo ng Gunting ang Paper");
-} else if (playerInput === "scissors" && computerInput === "rock") {
-  console.log("Talo Ka! Tinalo ng Bato ang Gunting");
-}
+  return weapons[randomIndex];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You Win";
+  } else {
+    return "You Lose";
+  }
+};
+
+const game = (rounds) => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  while (playerScore < rounds && computerScore < rounds) {
+    const playerSelection = getUserSelection();
+    const computerSelection = getComputerSelection();
+    const result = playRound(playerSelection, computerSelection);
+
+    console.log(`Player Selection: ${playerSelection}`);
+    console.log(`Computer Selection: ${computerSelection}`);
+    console.log(`Result: ${result}`);
+
+    if (result === "You Win") {
+      playerScore++;
+    } else if (result === "You Lose") {
+      computerScore++;
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Player won the games");
+  } else {
+    console.log("You lose! Try better next time");
+  }
+};
+
+game(3);
